adc17ae027f7a2710e6e0f832a036092dcec629d
JapaXZZ <ribeirodefigueredoeduardocesar@gmail.com>
2025-07-18 19:43:09 -0300

    Atualizar o postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-const config = {
-  plugins: [tailwindcss(), autoprefixer()],
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer()
+  ]
 };
-
-export default config;
