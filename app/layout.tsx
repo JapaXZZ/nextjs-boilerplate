fabd4081891b4060ece79c6dbfa6ad5e85f4e84c
JapaXZZ <ribeirodefigueredoeduardocesar@gmail.com>
2025-07-20 10:23:03 -0300

    Atualizar o layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   variable: "--font-geist-sans",
@@ -24,9 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${mono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${mono.variable} antialiased`}>
+        <Toaster richColors position="top-right" />
         {children}
       </body>
     </html>
