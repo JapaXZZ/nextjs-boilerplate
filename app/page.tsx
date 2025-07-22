53790929d308e7c80d5b46316b1eeb6f46f9476d
JapaXZZ <ribeirodefigueredoeduardocesar@gmail.com>
2025-07-21 21:28:52 -0300

    Atualizar o page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const initialScripts: Script[] = [
     title: "Tarefa SP",
     description: "Ferramenta avançada para automação e conclusão de tarefas",
     online: false,
-    url: "app/scripts/tarefa",
+    url: "/tarefa",
     category: "Web",
   },
   {
