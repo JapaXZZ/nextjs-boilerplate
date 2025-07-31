781d2875ca50e09cb88435bd9226cb1c0381cb52
JapaXZZ <ribeirodefigueredoeduardocesar@gmail.com>
2025-07-31 08:49:51 -0300

    Update page.tsx

--- app/matific/page.tsx
+++ app/matific/page.tsx
@@ -12,7 +12,7 @@ const TaskApp = () => {
   const { toast } = useToast();
   const router = useRouter();
 
-  const [scriptStatus, setScriptStatus] = useState<"online" | "offline">("offline");
+  const [scriptStatus, setScriptStatus] = useState<"online" | "offline">("online");
   const [lastUpdate] = useState("25/07/2025");
   const [url] = useState("https://matific.cupiditys.lol/");
   const [tasksCompleted] = useState(127);
