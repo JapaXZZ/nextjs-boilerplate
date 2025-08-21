faf1973c5b665d8fe5d579a8a89d22773aabb788
JapaXZZ <ribeirodefigueredoeduardocesar@gmail.com>
2025-08-21 08:13:00 -0300

    Update page.tsx

--- app/khan/page.tsx
+++ app/khan/page.tsx
@@ -122,7 +122,7 @@ const [sinalizadorcode, setSinalizadorcode] = useState<"online" | "offline">("on
      <Badge
   className={`px-3 py-1 ${
     sinalizadorcode === "offline"
-      ? "bg-red-600 text-white"
+      ? "bg-green-600 text-white"
       : "bg-red-600 text-white"
   }`}
 >
